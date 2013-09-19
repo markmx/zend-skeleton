f5fe7e587475ce792efb217678c3e20bc85a9a36
Mark Maynereid <mark.maynereid@gmail.com>
2013-09-19 16:26:15 +0100

    Switch over from Zend Db adapter to ZfcUserDoctrineORM adaptor module

--- config/application.config.php
+++ config/application.config.php
@@ -5,6 +5,9 @@ return array(
         'Application',
         'ZfcBase',
         'ZfcUser',
+        'ZfcUserDoctrineORM',
+        'DoctrineModule',
+        'DoctrineORMModule',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
